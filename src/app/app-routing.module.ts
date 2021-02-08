9fbfb40509cee41bbe05cabe31de7f50d3adf071
Anoy Dey <adanoy369@gmail.com>
2021-02-08 17:21:19 +0600

    created home-page component. under home-page component created home-banner component as a child component of home-page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {LoginComponent} from "./components/login/login.component";
-import {RegisterComponent} from "./components/register/register.component";
-import {AccountComponent} from "./components/account/account.component";
+import {LoginComponent} from "./components/core/login/login.component";
+import {RegisterComponent} from "./components/core/register/register.component";
+import {AccountComponent} from "./components/pages/account/account.component";
+import {HomePageComponent} from "./components/pages/home-page/home-page.component";
 
 const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'account', component: AccountComponent }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { RegisterComponent } from './components/register/register.component';
-import { AccountComponent } from './components/account/account.component';
-import { UserProfileComponent } from './components/account/user-profile/user-profile.component';
-import { AddressBookComponent } from './components/account/address-book/address-book.component';
-import { RecentOrdersComponent } from './components/account/recent-orders/recent-orders.component';
-import { AddressBookEditComponent } from './components/account/address-book/address-book-edit/address-book-edit.component';
-import { UserCoverComponent } from './components/account/user-profile/user-cover/user-cover.component';
-import { UserImageComponent } from './components/account/user-profile/user-cover/user-image/user-image.component';
-import { UserInfoComponent } from './components/account/user-profile/user-info/user-info.component';
-import { UserSocialLinkComponent } from './components/account/user-profile/user-social-link/user-social-link.component';
+import { LoginComponent } from './components/core/login/login.component';
+import { NavbarComponent } from './components/core/navbar/navbar.component';
+import { RegisterComponent } from './components/core/register/register.component';
+import { AccountComponent } from './components/pages/account/account.component';
+import { UserProfileComponent } from './components/pages/account/user-profile/user-profile.component';
+import { AddressBookComponent } from './components/pages/account/address-book/address-book.component';
+import { RecentOrdersComponent } from './components/pages/account/recent-orders/recent-orders.component';
+import { AddressBookEditComponent } from './components/pages/account/address-book/address-book-edit/address-book-edit.component';
+import { UserCoverComponent } from './components/pages/account/user-profile/user-cover/user-cover.component';
+import { UserImageComponent } from './components/pages/account/user-profile/user-cover/user-image/user-image.component';
+import { UserInfoComponent } from './components/pages/account/user-profile/user-info/user-info.component';
+import { UserSocialLinkComponent } from './components/pages/account/user-profile/user-social-link/user-social-link.component';
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
+import { HomeBannerComponent } from './components/pages/home-page/home-banner/home-banner.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +32,9 @@ import { UserSocialLinkComponent } from './components/account/user-profile/user-
     UserCoverComponent,
     UserImageComponent,
     UserInfoComponent,
-    UserSocialLinkComponent
+    UserSocialLinkComponent,
+    HomePageComponent,
+    HomeBannerComponent
   ],
   imports: [
     BrowserModule,
